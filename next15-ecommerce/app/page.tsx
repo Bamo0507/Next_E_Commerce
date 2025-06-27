190124b4bdb4b428c9f856d84010d78ef0df993c
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-06-26 20:41:43 -0600

    Add pagination to the products

--- next15-ecommerce/app/page.tsx
+++ next15-ecommerce/app/page.tsx
@@ -1,26 +1,69 @@
 import { ProductCard } from "./ProductCard";
 import { prisma } from "@/lib/prisma";
 
-export default async function HomePage() {
-  const products = await prisma.product.findMany();
-  console.log(products);
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export default async function HomePage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+
+  const page = Number(searchParams.page) || 1;
+  const pageSize = 3;
+  const skip = (page - 1) * pageSize;
+
+  const [products, total] = await Promise.all([
+    prisma.product.findMany({
+      skip,
+      take: pageSize,
+    }),
+    prisma.product.count(),
+  ]);
+
+  const totalPages = Math.ceil(total / pageSize);
+
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
   return (
-    <main className='container mx-auto p-4'>
-      <h1 className='text-3xl font-bold mb-6'>
-        Home
-      </h1>
-
-      <p className="mb-3">Showing {products.length} products</p>
-
-      {/* Grids of Products */}
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+      <p>Showing {products.length} products</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
-          // REMEMBER: key is important and should be unique, that is how we render a list in react
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+
+      <Pagination className="mt-8">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href={`?page=${page - 1}`} />
+          </PaginationItem>
+
+          {Array.from({ length: totalPages }, (_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink
+                href={`?page=${index + 1}`}
+                className={page === index + 1 ? "active" : ""}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          <PaginationItem>
+            <PaginationNext href={`?page=${page + 1}`} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </main>
   );
 }
